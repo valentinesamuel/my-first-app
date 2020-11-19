f6988b310efc1589e6aaca57b5bf7ff1dec68b58
Valentine <valentinesamuel2580@gmail.com>
2020-11-19 23:21:42 +0100

    Understood how components works and was ablr to finsh the first assignment

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './server.component.html',
+    selector: 'app-server',
+    styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+
+}
