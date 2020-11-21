7d0f311d00a44c4e1c430bc91ab36230119cab29
Valentine <valentinesamuel2580@gmail.com>
2020-11-22 00:40:12 +0100

    updated server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,17 +1,22 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './server.component.html',
-    selector: 'app-server',
-    styleUrls: ['./server.component.css']
+  templateUrl: './server.component.html',
+  selector: 'app-server',
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
-    serverid: number = 12;
-    serverStatus: string = 'online';
-    getServerStatus() {
-        return this.serverStatus;
-    }
-    
- 
+  serverid: number = 12;
+  serverStatus = 'offline';
+  getServerStatus() {
+    return this.serverStatus;
+  }
 
+  getColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
+  }
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
 }
